d3a715b873948cdabe1d182286c4d4030e343299
Sagar <sagar@app-scoop.com>
2020-09-07 18:10:37 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import ChatBot from './src/chatBot';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ChatBot />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/chatBot/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    question: 'hi',
+    answer: 'Hello',
+  },
+  {
+    question: 'Hello',
+    answer: 'Hi',
+  },
+  {
+    question: 'hi bot',
+    answer: 'Hello',
+  },
+  {
+    question: 'Hello bot',
+    answer: 'Hi',
+  },
+  {
+    question: 'how are you',
+    answer: 'I am fine',
+  },
+  {
+    question: 'What is your age',
+    answer: 'I was developed yesterday',
+  },
+  {
+    question: 'what is your name',
+    answer: 'I am bot',
+  },
+];
